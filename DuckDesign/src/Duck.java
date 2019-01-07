f4087ead24a1d11335deec3d898628c62d6bbf1c
Kai Wang <kk94wang@gmail.com>
2019-01-07 15:27:59 -0600

    code of HFDp

---
+++ DuckDesign/src/Duck.java
@@ -0,0 +1,28 @@
+public abstract class Duck {
+    QuackBehavior quackBehavior;
+    FlyBehavior flyBehavior;
+    public Duck() {
+    }
+
+    public abstract void display();
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
+    public void perfomFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+}
